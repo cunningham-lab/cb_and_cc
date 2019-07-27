62c6c9b473482a95d992996306eeee7cf20d107c
Gabriel Loaiza <multivac@Gabriels-MacBook-Pro.local>
2019-07-26 18:50:48 -0700

    log normalizing constant

--- utils.py
+++ utils.py
@@ -231,7 +231,7 @@ def cont_bern_log_norm(lam, l_lim=0.49, u_lim=0.51):
     # both options, regardless of the value of the condition.
     cut_lam = tf.where(tf.logical_or(tf.less(lam, l_lim), tf.greater(lam, u_lim)), lam, l_lim * tf.ones_like(lam))
     log_norm = tf.log(tf.abs(2.0 * tf.atanh(1 - 2.0 * cut_lam))) - tf.log(tf.abs(1 - 2.0 * cut_lam))
-    taylor = 4.0 / 3.0 * tf.pow(lam - 0.5, 2) + 104.0 / 45.0 * tf.pow(lam - 0.5, 4)
+    taylor = tf.log(2.0) + 4.0 / 3.0 * tf.pow(lam - 0.5, 2) + 104.0 / 45.0 * tf.pow(lam - 0.5, 4)
     return tf.where(tf.logical_or(tf.less(lam, l_lim), tf.greater(lam, u_lim)), log_norm, taylor)
 
 def mean_from_lam(lam):
