5651883fc14fbb39edc1b3bf79b658d48e40156e
ElliottGordon <elliott.gordon@hotmail.com>
2020-02-25 22:57:18 -0500

    Update cc_funcs.py

--- cc/cc_funcs.py
+++ cc/cc_funcs.py
@@ -1,8 +1,29 @@
 import tensorflow as tf
 import numpy as np
 
+
+def lambda_to_eta(lam):
+    eta = tf.math.log(lam / lam[:, -1, np.newaxis])
+    return eta[:, 0:-1]
+
+def eta_to_lambda(eta):
+    n = eta.shape[0]
+    aug_eta = tf.concat([eta, np.repeat(0.0, n)[:, np.newaxis]], -1)
+    return tf.math.softmax(aug_eta)
+
+
 def cc_log_normalizer(eta):
     # Compute the log-normalizer of the CC distribution
+    # See paper/supplement for mathematical details
+    n, K = eta.shape
+    dtype = eta.dtype
+    aug_eta = tf.concat([eta, tf.zeros([n,1], dtype=dtype)], -1)
+    lse = tf.math.reduce_logsumexp(aug_eta, axis=1)
+    res = - cc_log_norm_const(eta) + lse
+    return res
+
+def cc_log_norm_const(eta):
+    # Compute the log-normalizing-constant of the CC distribution
     # See paper/supplement for mathematical details
     n, K = eta.shape
     dtype = eta.dtype
@@ -11,15 +32,13 @@ def cc_log_normalizer(eta):
     scaling_factors = sds / 1.0
     aug_eta = tf.concat([eta, tf.zeros([n,1], dtype=dtype)], -1)
     lam = tf.math.softmax(aug_eta, axis=1)
-    lse = tf.math.reduce_logsumexp(aug_eta, axis=1)
     aug_eta = aug_eta / tf.reshape(scaling_factors, [n,1])
     rows = tf.reshape(aug_eta, [n, -1, 1])
     cols = tf.reshape(aug_eta, [n, -1, K+1])
     eta_diffs = rows - cols
     eta_diffs = eta_diffs + tf.eye(K+1, dtype=dtype)
     dens = tf.reduce_prod(eta_diffs, axis=1)
-    res = tf.math.log((-1)**K * tf.reduce_sum(lam / dens, axis=1)) +\
-        lse - \
+    res = - tf.math.log((-1)**K * tf.reduce_sum(lam / dens, axis=1)) +\
         K * tf.math.log(scaling_factors)
     return res
 
@@ -38,8 +57,12 @@ def cc_mean(eta):
     return res
 
 def cc_log_prob(sample, eta):
-    # Compute the normalizing constant
-    log_normalizer = cc_log_normalizer(eta)
     # Compute the likelihood term
-    loglik = tf.reduce_sum(sample[:, 0:-1] * eta, axis=1)
-    return loglik - log_normalizer
+    # loglik = tf.reduce_sum(sample[:, 0:-1] * eta, axis=1)
+    n, K = eta.shape
+    dtype = eta.dtype
+    aug_eta = tf.concat([eta, tf.zeros([n,1], dtype=dtype)], -1)
+    # lam = tf.math.softmax(aug_eta, axis=1)
+    loglik = - tf.nn.softmax_cross_entropy_with_logits(sample, aug_eta)
+    log_norm_const = cc_log_norm_const(eta)
+    return loglik + log_norm_const
